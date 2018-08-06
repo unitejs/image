959f8e393dda1006bdf824676c618fb3b099436a
Martyn Janes <martyn@obany.com>
2018-08-06 09:36:47 +0300

    Update dependencies

--- dist/icns.d.ts
+++ dist/icns.d.ts
@@ -2,6 +2,6 @@ import { IFileSystem } from "unitejs-framework/dist/interfaces/IFileSystem";
 import { ILogger } from "unitejs-framework/dist/interfaces/ILogger";
 export declare class ICNS {
     fromPng(logger: ILogger, fileSystem: IFileSystem, sourceFolder: string | undefined | null, sourceFile: string | undefined | null, destFolder: string | undefined | null, destFile: string): Promise<number>;
-    private imageToIcns(logger, sourceImage);
-    private appendChunk(sourceImage, outBuffer, type, size);
+    private imageToIcns;
+    private appendChunk;
 }
